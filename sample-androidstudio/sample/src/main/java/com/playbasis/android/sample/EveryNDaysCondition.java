186435c02072afc73f45dba99184ec7abdf01ca0
Kittikorn Ariyasuk <kittikorn.a@playbasis.com>
2015-10-20 15:35:18 +0700

    Engine rule condition: 1. Daily 2. Montly 3. Wekly 4. EveryNDays

---
+++ sample-androidstudio/sample/src/main/java/com/playbasis/android/sample/EveryNDaysCondition.java
@@ -0,0 +1,68 @@
+package com.playbasis.android.sample;
+
+import android.util.Log;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import com.playbasis.android.playbasissdk.model.Condition;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Created by TorIsHere on 10/20/2015 AD.
+ */
+public class EveryNDaysCondition extends Condition {
+
+    @Expose
+    @SerializedName("time_of_day")
+    private Date timeOfDay;
+
+    @Expose
+    @SerializedName("num_of_days")
+    private int numOfDays;
+
+    public Date getTimeOfDay() {
+        return timeOfDay;
+    }
+
+    public void setTimeOfDay(Date timeOfDay) {
+        this.timeOfDay = timeOfDay;
+    }
+
+    public int getNumOfDays() {
+        return numOfDays;
+    }
+
+    public void setNumOfDays(int numOfDays) {
+        this.numOfDays = numOfDays;
+    }
+
+    public static EveryNDaysCondition parseEveryNDays(JSONObject conditionJSON) throws JSONException {
+        EveryNDaysCondition everyNDaysCondition = new EveryNDaysCondition();
+
+        everyNDaysCondition.setName(conditionJSON.getString("name"));
+        everyNDaysCondition.setCategory(conditionJSON.getString("category"));
+        everyNDaysCondition.setDescription(conditionJSON.getString("description"));
+        everyNDaysCondition.setId(conditionJSON.getString("id"));
+        everyNDaysCondition.setSortOrder(conditionJSON.getInt("sort_order"));
+
+        try {
+            String dateTime = conditionJSON.getJSONObject("config").getString("time_of_day");
+            DateFormat format = new SimpleDateFormat("hh:mm", Locale.ENGLISH);
+            Date time = format.parse(dateTime);
+            everyNDaysCondition.setTimeOfDay(time);
+        } catch (ParseException e) {
+            Log.d("Engine Rule Condition", e.getMessage());
+        }
+        everyNDaysCondition.setNumOfDays(conditionJSON.getJSONObject("config").getInt("num_of_days"));
+
+        return everyNDaysCondition;
+    }
+}
---
+++ sample-androidstudio/sample/src/main/java/com/playbasis/android/sample/WeeklyCondition.java
@@ -0,0 +1,68 @@
+package com.playbasis.android.sample;
+
+import android.util.Log;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import com.playbasis.android.playbasissdk.model.Condition;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Created by TorIsHere on 10/20/2015 AD.
+ */
+public class WeeklyCondition extends Condition {
+
+    @Expose
+    @SerializedName("time_of_day")
+    private Date timeOfDay;
+
+    @Expose
+    @SerializedName("day_of_week")
+    private String dayOfWeek;
+
+    public Date getTimeOfDay() {
+        return timeOfDay;
+    }
+
+    public void setTimeOfDay(Date timeOfDay) {
+        this.timeOfDay = timeOfDay;
+    }
+
+    public String getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public void setDayOfWeek(String dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    public static WeeklyCondition parseWeeklyCondition(JSONObject conditionJSON) throws JSONException {
+        WeeklyCondition weeklyCondition = new WeeklyCondition();
+
+        weeklyCondition.setName(conditionJSON.getString("name"));
+        weeklyCondition.setCategory(conditionJSON.getString("category"));
+        weeklyCondition.setDescription(conditionJSON.getString("description"));
+        weeklyCondition.setId(conditionJSON.getString("id"));
+        weeklyCondition.setSortOrder(conditionJSON.getInt("sort_order"));
+
+        try {
+            String dateTime = conditionJSON.getJSONObject("config").getString("time_of_day");
+            DateFormat format = new SimpleDateFormat("hh:mm", Locale.ENGLISH);
+            Date time = format.parse(dateTime);
+            weeklyCondition.setTimeOfDay(time);
+        } catch (ParseException e) {
+            Log.d("Engine Rule Condition", e.getMessage());
+        }
+        weeklyCondition.setDayOfWeek(conditionJSON.getJSONObject("config").getString("day_of_month"));
+
+        return weeklyCondition;
+    }
+}
