2b20f13016b66ebeca36618a85415dc7a764417c
Kittikorn Ariyasuk <kittikorn.a@playbasis.com>
2016-01-07 18:17:48 +0700

    fine compatibility with store org on Node class

--- sample-androidstudio/library/src/main/java/com/playbasis/android/playbasissdk/model/Node.java
+++ sample-androidstudio/library/src/main/java/com/playbasis/android/playbasissdk/model/Node.java
@@ -8,24 +8,57 @@ import com.google.gson.annotations.SerializedName;
  */
 public class Node {
 
-    @SerializedName("node_id")
+    @SerializedName("_id")
     @Expose
-    private String nodeId;
+    String id;
 
     @Expose
-    private String name;
+    String name;
+
+    @Expose
+    String description;
+
+    @Expose
+    boolean status;
+
+    @Expose
+    String slug;
+
+    @SerializedName("date_added")
+    @Expose
+    String dateAdded;
+
+
+    @SerializedName("date_modified")
+    @Expose
+    String dateModified;
+
+    String parentId;
+    String parentName;
+
+    String organizeId;
+    String organizeName;
 
     public Node() {
-        this.nodeId = "";
+        this.id = "";
         this.name = "";
+        this.description = "";
+        this.status = false;
+        this.slug = "";
+        this.dateAdded = "";
+        this.dateModified = "";
+        this.parentId = "";
+        this.parentName = "";
+        this.organizeId = "";
+        this.organizeName = "";
     }
 
-    public String getNodeId() {
-        return nodeId;
+    public String getId() {
+        return id;
     }
 
-    public void setNodeId(String nodeId) {
-        this.nodeId = nodeId;
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -35,4 +68,77 @@ public class Node {
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public String getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(String dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+    public String getDateModified() {
+        return dateModified;
+    }
+
+    public void setDateModified(String dateModified) {
+        this.dateModified = dateModified;
+    }
+
+    public String getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(String parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getParentName() {
+        return parentName;
+    }
+
+    public void setParentName(String parentName) {
+        this.parentName = parentName;
+    }
+
+    public String getOrganizeId() {
+        return organizeId;
+    }
+
+    public void setOrganizeId(String organizeId) {
+        this.organizeId = organizeId;
+    }
+
+    public String getOrganizeName() {
+        return organizeName;
+    }
+
+    public void setOrganizeName(String organizeName) {
+        this.organizeName = organizeName;
+    }
+
 }
